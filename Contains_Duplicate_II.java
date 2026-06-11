c8335e0b6a146ad353fd262184f7082f8fbc50fb
Sumedha Parashar <sumedhaparashar6@gmail.com>
2026-06-11 21:51:05 +0530

    Add files via upload

---
+++ Contains_Duplicate_II.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+
+public class Contains_Duplicate_II {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer,Integer> map= new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            
+            if(map.containsKey(nums[i]) && Math.abs(i-map.get(nums[i]))<=k){
+                return true;
+            }map.put(nums[i],i);
+        }return false;
+        
+    }
+}
